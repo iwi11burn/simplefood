570423335f12afa6968ceec8f02b7c11eec45ffa
iwi11burn <iwillburn11@icloud.com>
2025-01-05 12:53:11 +0200

    add hw2

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,13 @@
-$(function () {
-	return
+$('.reviews__slider').slick({
+	dots: true,
+	responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+			},
+		},
+	],
 })
 
 var mixer = mixitup('.popular-categories__list')
--- gulpfile.js
+++ gulpfile.js
@@ -1,90 +1,132 @@
-const { src, dest, watch, parallel, series } = require('gulp')
-const del = require('del')
-const sass = require('gulp-sass')(require('sass'))
-const uglify = require('gulp-uglify')
-const browserSync = require('browser-sync').create()
-const concat = require('gulp-concat')
-const postcss = require('gulp-postcss')
-const imagemin = require('gulp-imagemin')
-const autoprefixer = require('autoprefixer')
+const { src, dest, watch, parallel, series } = require("gulp");
+const scss = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const autoprefixer = require("gulp-autoprefixer");
+const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
+const browserSync = require("browser-sync").create();
+const replace = require('gulp-replace');
+const cheerio = require('gulp-cheerio');
+const svgSprite = require('gulp-svg-sprite');
+const fileInclude   = require('gulp-file-include');
+
 
 function browsersync() {
-	browserSync.init({
-		server: {
-			baseDir: './app/',
-		},
-		notify: false,
-	})
+  browserSync.init({
+    server: {
+      baseDir: "app/",
+    },
+    notify: false,
+  });
+}
+
+const htmlInclude = () => {
+  return src(['app/html/**/*.html']) // Находит любой .html файл в папке "html", куда будем подключать другие .html файлы													
+  .pipe(fileInclude({
+    prefix: '@',
+    basepath: '@file',
+  }))
+  .pipe(dest('app')) // указываем, в какую папку поместить готовый файл html
+  .pipe(browserSync.stream());
+}
+
+function svgSprites() {
+  return src('app/images/icons/*.svg') 
+  .pipe(cheerio({
+        run: ($) => {
+            $("[fill]").removeAttr("fill"); 
+            $("[stroke]").removeAttr("stroke"); 
+            $("[style]").removeAttr("style"); 
+        },
+        parserOptions: { xmlMode: true },
+      })
+  )
+	.pipe(replace('&gt;','>')) // боремся с заменой символа 
+	.pipe(
+	      svgSprite({
+	        mode: {
+	          stack: {
+	            sprite: '../sprite.svg', 
+	          },
+	        },
+	      })
+	    )
+	.pipe(dest('app/images')); 
 }
 
 function styles() {
-	return src('./app/scss/style.scss')
-		.pipe(sass({ outputStyle: 'compressed' }))
-		.pipe(
-			postcss([
-				autoprefixer({
-					overrideBrowserslist: ['last 10 versions'],
-					grid: 'autoplace',
-				}),
-			])
-		)
-		.pipe(concat('style.min.css'))
-		.pipe(dest('./app/css'))
-		.pipe(browserSync.stream())
+  return src("app/sass/main.scss")
+    .pipe(scss({ outputStyle: "compressed" }))
+    .pipe(concat("style.min.css"))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 10 versions"],
+        grid: true,
+      })
+    )
+    .pipe(dest("app/css"))
+    .pipe(browserSync.stream());
 }
 
 function scripts() {
-	return src(['./node_modules/jquery/dist/jquery.js', './app/js/main.js'])
-		.pipe(concat('main.min.js'))
-		.pipe(uglify())
-		.pipe(dest('./app/js'))
+  return src(["node_modules/jquery/dist/jquery.js", "node_modules/mixitup/dist/mixitup.min.js", "node_modules/fancybox/dist/js/jquery.fancybox.js", "node_modules/slick-carousel/slick/slick.min.js","app/js/main.js"])
+    .pipe(concat("main.min.js"))
+    .pipe(uglify())
+    .pipe(dest("app/js"))
+    .pipe(browserSync.stream());
 }
 
 function images() {
-	return src('./app/images/**/*.*')
-		.pipe(
-			imagemin([
-				imagemin.gifsicle({ interlaced: true }),
-				imagemin.mozjpeg({ quality: 75, progressive: true }),
-				imagemin.optipng({ optimizationLevel: 5 }),
-				imagemin.svgo({
-					plugins: [
-						{
-							removeViewBox: true,
-						},
-						{
-							cleanupIDs: false,
-						},
-					],
-				}),
-			])
-		)
-		.pipe(dest('./dist/images'))
+  return src("app/images/**/*.*", { encoding: false })
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [
+            {
+              name: "removeViewBox",
+              active: true,
+            },
+            {
+              name: "cleanupIDs",
+              active: false,
+            },
+          ],
+        }),
+      ])
+    )
+    .pipe(dest("dist/images"));
 }
 
+
 function build() {
-	return src(
-		['./app/**/*.html', './app/css/style.min.css', './app/js/main.min.js'],
-		{ base: './app' }
-	).pipe(dest('./dist'))
+  return src(["app/**/*.html", "app/css/style.min.css", "app/js/main.min.js"], {
+    base: "app",
+  }).pipe(dest("dist"));
 }
 
 function cleanDist() {
-	return del('./dist/**')
+  return del("dist");
 }
 
 function watching() {
-	watch(['./app/scss/**/*.scss'], styles)
-	watch(['./app/js/**/*.js', '!./app/js/main.min.js'], scripts)
-	watch(['./app/**/*.html']).on('change', browserSync.reload)
+  watch(['app/html/**/*.html'], htmlInclude);
+	watch(['app/images/icons/*.svg'], svgSprites);
+  watch(["app/sass/**/*.scss"], styles);
+  watch(["app/js/**/*.js", "!app/js/main.min.js"], scripts);
+  watch(["app/**/*.html"]).on("change", browserSync.reload);
 }
 
-exports.browsersync = browsersync
-exports.styles = styles
-exports.scripts = scripts
-exports.images = images
-exports.watching = watching
-exports.cleanDist = cleanDist
-exports.build = series(cleanDist, images, build)
+exports.styles = styles;
+exports.scripts = scripts;
+exports.browsersync = browsersync;
+exports.watching = watching;
+exports.images = images;
+exports.cleanDist = cleanDist;
+exports.svgSprites = svgSprites;
+exports.build = series(cleanDist, images, build);
 
-exports.default = parallel(browsersync, styles, scripts, watching, images)
+exports.default = parallel(htmlInclude, svgSprites, styles, scripts, browsersync, watching);
