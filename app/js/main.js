28e3139d5fa25902a707810341bc8631c5deeb94
iwi11burn <iwillburn11@icloud.com>
2025-01-06 11:03:55 +0200

    add slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,12 @@
 $('.reviews__slider').slick({
 	dots: true,
-	responsive: [
-		{
-			breakpoint: 768,
-			settings: {
-				arrows: false,
-			},
-		},
-	],
+	prevArrow:
+		'<button type="button" class="slick-prev"><img class="slick-icon icon" src="../images/sprite.svg#icon-left" alt=""/></button>',
+	nextArrow:
+		'<button type="button" class="slick-next"><img class="slick-icon icon" src="../images/sprite.svg#icon-left" alt=""/></button>',
+	customPaging: function (slider, i) {
+		return '<button type="button"></button>'  
+	},
 })
 
 var mixer = mixitup('.popular-categories__list')
