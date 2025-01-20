5c239a821e81bc24ae08ca526ea0d3869ea39d3a
iwi11burn <iwillburn11@icloud.com>
2025-01-20 11:38:49 +0200

    add adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,54 @@
 $('.reviews__slider').slick({
 	dots: true,
-	prevArrow:
-		'<button type="button" class="slick-prev"><img class="slick-icon icon" src="../images/sprite.svg#icon-left" alt=""/></button>',
-	nextArrow:
-		'<button type="button" class="slick-next"><img class="slick-icon icon" src="../images/sprite.svg#icon-left" alt=""/></button>',
+	infinite: false,
+	prevArrow: `
+			<button type="button" class="slick-prev">
+					<svg class="slick-icon icon" width="11" height="19">
+							<use xlink:href="../images/sprite.svg#icon-left"></use>
+					</svg>
+			</button>`,
+	nextArrow: `
+			<button type="button" class="slick-next">
+					<svg class="slick-icon icon" width="11" height="19">
+							<use xlink:href="../images/sprite.svg#icon-left"></use>
+					</svg>
+			</button>`,
 	customPaging: function (slider, i) {
-		return '<button type="button"></button>'  
+		return '<button type="button"></button>'
 	},
+})
+
+$(document).ready(function () {
+	let slickInitialized = false // Флаг для проверки инициализации слайдера
+
+	function initSlickSlider() {
+		const screenWidth = $(window).width() // Определяем ширину экрана
+
+		if (screenWidth <= 768) {
+			// Слайдер включается только на экранах <= 768px
+			if (!slickInitialized) {
+				$('.restaurant__list').slick({
+					dots: true,
+					arrows: false,
+					infinite: true,
+					centerMode: true,
+					centerPadding: '15',
+					slidesToShow: 1,
+				})
+				slickInitialized = true
+			}
+		} else {
+			if (slickInitialized) {
+				// Если слайдер был инициализирован, удаляем его
+				$('.restaurant__list').slick('unslick')
+				slickInitialized = false // Сбрасываем флаг
+			}
+		}
+	}
+
+	// Вызываем функцию при загрузке страницы и изменении размера экрана
+	initSlickSlider()
+	$(window).resize(initSlickSlider)
 })
 
 var mixer = mixitup('.popular-categories__list')
